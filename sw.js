8235f130e8f606390e706f7b2fbf9b9360437086
jacobmathiasstache-cloud <jacob.mathias.stache@gmail.com>
2026-09-14 20:03:49 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'kopfrechnen-v1';
+const CACHE = 'kopfrechnen-v2';
 const FILES = ['./', './index.html', './icon.png', './icon-512.png', './manifest.json'];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)).then(() => self.skipWaiting()));
